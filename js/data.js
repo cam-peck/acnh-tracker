b3c87e0ebbfae917c50b803936b33140e323c0a6
Cameron <cameronpeck317@gmail.com>
2022-09-27 14:51:16 +0000

    add data model to data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,16 @@
 /* exported data */
+var data = {
+  view: '',
+  towns: [],
+  editing: null,
+  nextEntryId: 1
+};
+
+// var town = {
+//   imageUrl: '',
+//   playerName: '',
+//   townName: '',
+//   townFruit: '',
+//   townVillagers: [],
+//   entryId: 0
+// }
