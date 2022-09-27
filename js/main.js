e97c5bf2bfa248f21af4dddaa42eeedea443589d
Cameron <cameronpeck317@gmail.com>
2022-09-27 23:30:41 +0000

    Listen for click events on the fruitSelection parent to update the currently selected fruit background to a light yellow #1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var $fruitContainer = document.querySelector('.fruit-container');
+var $fruits = document.querySelectorAll('.fruit-img');
+
+$fruitContainer.addEventListener('click', handleFruitClick);
+
+function handleFruitClick(event) {
+  if (event.target.tagName === 'INPUT') {
+    var parentDiv = (event.target.closest('div'));
+    var labelChild = parentDiv.firstElementChild;
+    var fruitImg = labelChild.firstElementChild;
+    for (let i = 0; i < $fruits.length; i++) {
+      if ($fruits[i] === fruitImg) {
+        $fruits[i].classList.add('light-yellow-bg');
+      } else {
+        $fruits[i].className = 'fruit-img';
+      }
+    }
+  }
+}
