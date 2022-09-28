fe1b58147fbc9d41a2d24685e648b94b421b7319
Cameron <cameronpeck317@gmail.com>
2022-09-28 15:10:23 +0000

    Verify that new entries are saved to local storage in the towns array along with the rest of the data model on page reload #1

--- js/data.js
+++ js/data.js
@@ -7,6 +7,15 @@ var data = {
   currentVillagers: []
 };
 
+var previousData = localStorage.getItem('acnh-tracker-data');
+if (previousData !== null) {
+  data = JSON.parse(previousData);
+}
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('acnh-tracker-data', dataJSON);
+});
+
 // var town = {
 //   imageUrl: '',
 //   playerName: '',
